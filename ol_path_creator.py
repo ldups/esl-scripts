173bbc8d0c4011d6b50c412ed1b2ece90761cae5
Louise Dupont <louisesdupont@gmail.com>
2022-09-08 13:54:02 -0400

    Continued working on reading tree file

--- ol_path_creator.py
+++ ol_path_creator.py
@@ -1,24 +1,45 @@
 class Term:
-    def __init__(self, id, name, defin, is_a):
+    def __init__(self, id, name, is_a):
         self.id = id
-        #self.name = name
-        #self.defin = defin
-        #self.parent = is_a
+        self.name = name
+        self.parent = is_a
 
-treeFile = open("MPheno_OBO.ontology.txt", "r")
-searchTerm = "is_a: MP:0000001"
-#lines = treeFile.readlines()
+# reads tree file and splits lines into a list
+treeFile = open('MPheno_OBO.ontology.txt', 'r')
 treeContent = treeFile.read().splitlines()
-termList = []
-for i in range(len(treeContent)):
-    line = treeContent[i]
-    if line.find("[Term]") != -1:
-        id = treeContent[i+1]
-        termList.append(Term(id))
+treeFile.close()
 
+searchTerm = 'is_a: MP:0000001'
+#lines = treeFile.readlines()
+
+# removes empty and whitespace lines from list
+for line in treeContent:
+    if not line or line.isspace():
+        treeContent.remove(line)
+
+# finds indeces of all occurences of '[Term]' in list
+termIndexList = []
+for i in range(len(treeContent)):
+    if treeContent[i].find('[Term]') != -1:
+        termIndexList.append(i)
+
+termList = []
+for i in range(len(termIndexList)):
+    termIndex = termIndexList[i]
+    # excludes top level group
+    if treeContent[termIndex + 1] != "id: MP:0000001":
+        id = treeContent[termIndex+1]
+        name = treeContent[termIndex+2]
+        if termIndex == len(termList) - 1:
+            is_a = treeContent[len(treeContent) - 1]
+        else:
+            is_a = treeContent[termIndexList[i+1]-1]
+        termList.append(Term(id, name, is_a))
+
+print(termList[1].id)
 
 #print(treeContent)
-treeFile.close()
+
 
 # too slow
 '''regEx = re.compile("\[Term\]\n([\s\S]+is_a: MP:0000001)")
