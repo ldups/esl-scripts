047646b2160e31b316166e676ef7f3e13702c5f0
Louise Dupont <louisesdupont@gmail.com>
2022-09-14 16:37:26 -0400

    Removed recursive search for now

--- create_path_file.py
+++ create_path_file.py
@@ -1,7 +1,6 @@
 import re
 import time
-from match_gene_groups import match_gene_groups
-from match_gene_groups import Search_method
+from match_gene_groups_top_level import match_gene_groups_top_level
 
 ontology_file_name = 'MPheno_OBO.ontology.txt'
 
@@ -50,10 +49,10 @@ def create_path_file(all_paths_file_name, gene_data_file_name):
     path_dictionary = parse_full_path_file(all_paths_file_name)
     gene_dictionary = read_gene_data_file(gene_data_file_name)
 
-    final_path_file = open('output_path_file_level_4.txt', 'w')
-    group_key_file = open('output_key_file.txt_level_4', 'w')
+    final_path_file = open('output_path_file_level_1.txt', 'w')
+    group_key_file = open('output_key_file_level_1.txt', 'w')
 
-    matching_groups = match_gene_groups('MPheno_OBO.ontology.txt', Search_method.LEVEL, search_term = None, level = 4)
+    matching_groups = match_gene_groups_top_level('MPheno_OBO.ontology.txt')
     group_dictionary = {}
     for group in matching_groups:
         group_dictionary[group.id] = []
--- match_gene_groups_top_level.py
+++ match_gene_groups_top_level.py
@@ -1,28 +1,21 @@
 from build_ontology_term_list import build_ontology_term_list
 from enum import Enum
 
-class Search_method(Enum):
+""" class Search_method(Enum):
     IS_A = 1
     LEVEL = 2
-    LEVEL_RECURSIVE = 3
+    LEVEL_RECURSIVE = 3 """
 
 
-def match_gene_groups(file_name, method, search_term = None, level = None):
+def match_gene_groups_top_level(file_name):
     term_list = build_ontology_term_list(file_name)
     matching_category_list = []
 
-    if method == search_term:
+    search_term = 'MP:0000001'
 
-        search_term = 'MP:0000001'
-
-        for term in term_list:
-            if term.parent == search_term:
-                matching_category_list.append(term)
-    
-    elif method == Search_method.LEVEL:
-        for term in term_list:
-            if find_group_level(term, term_list) == level:
-                matching_category_list.append(term)
+    for term in term_list:
+        if term.parent == search_term:
+            matching_category_list.append(term)
 
     return matching_category_list
 
@@ -43,6 +36,5 @@ def find_group_level(group, term_list):
     
     return find_group_level(parent_object, term_list) + 1
 
-match_gene_groups('MPheno_OBO.ontology.txt', Search_method.LEVEL, search_term = None, level = 4)
 
  
