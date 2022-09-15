eb522206fdf33a894a703c698af296977550832e
Louise Dupont <louisesdupont@gmail.com>
2022-09-15 16:46:35 -0400

    Function to relate gene names to MGI to MP

--- read_MGI_GenePheno.py
+++ read_MGI_GenePheno.py
@@ -1,25 +1,48 @@
-def read_MGI_GenePheno(geno_pheno_name, pheno_ont_name):
+from unicodedata import name
 
+
+def read_MGI_GenePheno(geno_pheno_name, pheno_ont_name):
+    ''' 
+    reads geno_pheno file and pheno_ontology file and creates dictionary relating gene name and lowest level mammalian phenotype ids
+    argument 1: geno_pheno_name: file name for file relating MGI and lowest level MP
+    argument 2: pheno_ont_name: file name for file relating MGI and gene name
+    returns dictionary in form {gene name: [list of lowest level MPs]}
+    '''
+    # in current case, gene_pheno_file = MGI_GenePheno.rpt.txt
     gene_pheno_file = open(geno_pheno_name, 'r')
+    # in current case, pheno_ont_file = MGI_phenotype_ontology_data.txt
     pheno_ont_file = open(pheno_ont_name, 'r')
-    mp_mgi_dictionary = {}
+
+    # creates dictionary from gene_pheno file in form {mgi:[mp]}
+    mgi_mp_dictionary = {}
     for line in gene_pheno_file:
         mp = line.split('\t')[4]
-        mgis = line.split('\t')[5].split('|')
+        mgis = line.split('\t')[6].split('|')
         for mgi in mgis:
-            mp_mgi_dictionary[mgi] = mp
+            if mgi not in mgi_mp_dictionary:
+                mgi_mp_dictionary[mgi] = []
+            mgi_mp_dictionary[mgi].append(mp)
 
+    mgi_name_dictionary = {}
+    # skips header line
+    next(pheno_ont_file)
     for line in pheno_ont_file:
-        
-    
+        gene_name = line.split('\t')[0]
+        mgi = line.split('\t')[3]
+        mgi_name_dictionary[mgi] = gene_name
 
-        
-
-
-
-
+    gene_mpi_dictionary = {}
+    for mgi in mgi_mp_dictionary:
+        if mgi in mgi_name_dictionary:
+            gene_name = mgi_name_dictionary[mgi]
+            mp = mgi_mp_dictionary[mgi]
+            if gene_name not in gene_mpi_dictionary:
+                gene_mpi_dictionary[gene_name] = []
+            gene_mpi_dictionary[gene_name].extend(mp)
 
     gene_pheno_file.close()
     pheno_ont_file.close()
 
-read_MGI_GenePheno('MGI_GenePheno.rpt.txt', 'MGI_phenotype_ontology_data.txt')
+    return gene_mpi_dictionary
+
+
